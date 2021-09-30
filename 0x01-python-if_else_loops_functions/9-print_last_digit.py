ff494d945fc5109d1e82a5454cd2f7ef11b0e40f
ADEOLU AWOTUNBO <adeoluawotunbo@gmail.com>
2021-09-30 07:07:53 -0700

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number < 0:
-        last_digit = number % -10
+        number = (number * -1) % 10
     else:
-        last_digit = number % 10
-        print("{}".format(last_digit), end="")
-    return last_digit
+        number = number % 10
+    print(number, end="")
+    return (number)
